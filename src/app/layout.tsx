a15f198be6c9c14581b8339b2029fad12122b58e
Michal <noszczykmichal@wp.pl>
2025-03-20 15:33:26 +0100

    save before shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,56 +31,61 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.variable} antialiased`}>
-        <header className="flex justify-between text-lg py-[46px] px-[80px]">
-          <Link href="/">
-            <Logo className="fill-[var(--gray-100)]" />
-          </Link>
-          <ul className="flex">
-            {linksConfig.map((link) => (
-              <li key={link.id} className="px-[20px]">
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
-          <a
-            href="telto:+48508832553"
-            className={`${montserrat.variable} font-bold`}
-          >
-            +48 508 832 553
-          </a>
-        </header>
-
-        <main className="px-[80px">{children}</main>
-        <footer className="bg-[var(--black-100)] mx-[80px] rounded-[30px] text-[var(--white-100)] px-[50px] py-[65px] flex gap-[48px] text-[15px] leading-[26px]">
-          <div className="w-1/3">
+        <header className="text-lg">
+          <nav className="max-w-[1300px] mx-auto flex justify-between py-[46px]">
             <Link href="/">
-              <Logo className="fill-[var(--white-100)]" />
+              <Logo className="fill-[var(--gray-100)]" />
             </Link>
-          </div>
-          <div className="w-1/3 flex flex-col justify-between">
-            <div>
-              <p className="font-semibold">Adres</p>
-              <p>ul. Fort Służew 3 lok. U2</p>
-              <p>02-787 Warszawa</p>
-            </div>
-            <div>
-              <p className="font-semibold">Telefon</p>
-              <p>+48 792 044 090</p>
-            </div>
-            <div>
-              <p className="font-semibold">E-mail</p>
-              <p>gabinet@platinumkosmetologia.pl</p>
-            </div>
-          </div>
-          <div className="w-1/3">
-            <ul className="list-none">
+            <ul className="flex">
               {linksConfig.map((link) => (
-                <li key={link.id} className="">
+                <li key={link.id} className="px-[20px]">
                   <Link href={link.href}>{link.label}</Link>
                 </li>
               ))}
             </ul>
-          </div>
+            <a
+              href="telto:+48508832553"
+              className={`${montserrat.variable} font-bold`}
+            >
+              +48 508 832 553
+            </a>
+          </nav>
+        </header>
+
+        <main className="max-w-[1300px] mx-auto">{children}</main>
+
+        <footer className="text-[var(--white-100)] text-[15px] leading-[26px]">
+          <section className="max-w-[1300px] h-[100%] mx-auto px-[50px] py-[65px] flex gap-[48px] bg-[var(--black-100)] rounded-[var(--primary-border-radius)]">
+            <div className="w-1/3">
+              <Link href="/">
+                <Logo className="fill-[var(--white-100)]" />
+              </Link>
+            </div>
+            <div className="w-1/3 flex flex-col justify-between">
+              <div>
+                <p className="font-semibold">Adres</p>
+                <p>ul. Fort Służew 3 lok. U2</p>
+                <p>02-787 Warszawa</p>
+              </div>
+              <div>
+                <p className="font-semibold">Telefon</p>
+                <p>+48 792 044 090</p>
+              </div>
+              <div>
+                <p className="font-semibold">E-mail</p>
+                <p>gabinet@platinumkosmetologia.pl</p>
+              </div>
+            </div>
+            <div className="w-1/3">
+              <ul className="list-none">
+                {linksConfig.map((link) => (
+                  <li key={link.id} className="">
+                    <Link href={link.href}>{link.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
         </footer>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,62 @@
+import Image from "next/image";
+
+import pkBannerLeft from "@/assets/pk-banner-left.jpg";
+import pkBannerRight from "@/assets/pk-banner-right.jpg";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <section className="max-w-[1300px] mx-auto grid gap-y-[100px]">
+      <div className="flex rounded-[var(--primary-border-radius)] overflow-hidden max-h-[500px]">
+        <div
+          className="w-[50%] flex justify-center items-center"
+          style={{ backgroundImage: `url(${pkBannerLeft.src})` }}
+        >
+          <h1
+            style={{
+              fontSize: "var(--font-size-banner)",
+              lineHeight: "var(--font-size-banner)",
+            }}
+            className="w-[60%] font-extralight"
+          >
+            Kosmetologia na światowym poziomie
+          </h1>
+        </div>
+        <Image
+          src={pkBannerRight}
+          alt=""
+          width={0}
+          height={0}
+          className="w-[50%] object-cover"
+        />
+      </div>
+      <article>
+        <div className="flex gap-x-[50px]">
+          <h3
+            style={{
+              fontSize: "var(--font-size-heading)",
+            }}
+            className="w-[50%] font-extralight"
+          >
+            Zabiegi szyte na miarę potrzeb Twojej skóry
+          </h3>
+          <div className="w-[50%] text-[17px]">
+            <p className="pb-[26px]">
+              W naszym gabinecie realizujemy zabiegi szyte na miarę potrzeb
+              Twojej skóry. Zespół wybitnych specjalistów w zakresie medycyny
+              estetycznej oraz kosmetologii połączył siły, by w sercu Gocławia w
+              Warszawie stworzyć wyjątkowe miejsce dla najbardziej wymagających
+              klientów.
+            </p>
+            <p>
+              Dzięki wieloletniemu doświadczeniu naszych lekarzy i kosmetologów
+              tworzymy terapie dostosowane do wieku, problemu oraz oczekiwań
+              klienta. W zabiegach wykorzystujemy najnowsze osiągnięcia medycyny
+              i kosmetologii, nie zapominając przy tym o podstawach, jakimi są
+              bezpieczeństwo oraz skuteczność podjętych terapii skóry.
+            </p>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
