5f6b884ef5823d44686d584247ffaa26136a47f6
Michal <noszczykmichal@wp.pl>
2025-03-27 19:13:43 +0100

    added new dependency - the simple icons, new components the booksy and logo, updated navigationa nad footer

--- src/components/Layout/Navigation/Navigation.tsx
+++ src/components/Layout/Navigation/Navigation.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
 import { Smartphone } from "lucide-react";
 
 import { UIContextProvider } from "@/store/uiContext";
-import Logo from "@/components/Icons/Logo";
+import Icon from "@/components/Layout/Icon/Icon";
 import NavigationItems from "./NavigationItems/NavigationItems";
 import Hamburger from "./Hamburger/Hamburger";
 import SideNav from "./SideNav/SideNav";
@@ -16,13 +15,11 @@ function Navigation({ classNames }: NavigationProps) {
     <UIContextProvider>
       <header className={`w-full flex items-center text-lg ${classNames}`}>
         <nav className="w-[100%] px-[10px] md:px-[42px] py-[20px] flex justify-between">
-          <Link href="/" id="header-logo">
-            <Logo className="fill-[var(--gray-100)] w-[100%]" />
-          </Link>
+          <Icon name="logo" href="/" className="h-[40px]" id="header-logo" />
           <NavigationItems className="hidden lg:flex h-full" />
           <div className="ml-[10px] flex items-center justify-between lg:justify-end flex-grow-[0.5] max-w-[250px]">
             <a
-              href="telto:+48508832553"
+              href="tel:+48508832553"
               className="flex items-center text-nowrap py-2 px-4 font-extralight text-[15px] bg-[var(--gray-75)] rounded-[var(--big-border-radius)] mr-2"
             >
               <Smartphone className="stroke-1" /> Umów wizytę
