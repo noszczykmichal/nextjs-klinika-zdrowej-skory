2bea207ba5740b0982a0852dab60f30aad9a6fd7
Michal <noszczykmichal@wp.pl>
2025-05-29 12:25:52 +0200

    modified config for eslint, new plugin for prettier

--- src/utils/portableTextComponentConfig.tsx
+++ src/utils/portableTextComponentConfig.tsx
@@ -6,7 +6,7 @@ import { urlFor } from "./utilityFunctions";
 export const portableTextComponentConfig: PortableTextComponents = {
   marks: {
     underline: ({ children }) => (
-      <span className="border-b-1 border-amber-800 pb-1">{children}</span>
+      <span className="border-b border-amber-800 pb-1">{children}</span>
     ),
   },
   types: {
@@ -21,7 +21,7 @@ export const portableTextComponentConfig: PortableTextComponents = {
             alt={value.alt}
             width={width}
             height={height}
-            className="w-full h-auto object-contain"
+            className="h-auto w-full object-contain"
           />
         </div>
       );
@@ -30,14 +30,14 @@ export const portableTextComponentConfig: PortableTextComponents = {
   block: {
     normal: ({ children }) => <p className="mb-[16px]">{children}</p>,
     h3: ({ children }) => (
-      <h3 className="mt-[40px] mb-[8px] text-[25px] leading-[1.5]">
+      <h3 className="mt-[40px] mb-[8px] text-[25px] leading-normal">
         {children}
       </h3>
     ),
   },
   list: {
     bullet: ({ children }) => (
-      <ul className="pl-[20px] list-disc space-y-[6px] my-[16px]">
+      <ul className="my-[16px] list-disc space-y-[6px] pl-[20px]">
         {children}
       </ul>
     ),
