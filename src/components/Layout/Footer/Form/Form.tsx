508cfbaeb024b769cbff20cb23d1d06406d8a013
Michal <noszczykmichal@wp.pl>
2025-06-10 19:23:44 +0200

    added new component Loader

--- src/components/Layout/Footer/Form/Form.tsx
+++ src/components/Layout/Footer/Form/Form.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { useState } from "react";
+import dynamic from "next/dynamic";
 import { Form } from "react-final-form";
 
 import { formConfig } from "@/utils/config";
 import FormField from "@/components/Layout/Footer/Form/FormField/FormField";
+import Loader from "@/components/ui/custom/Loader/Loader";
+
+const Countdown = dynamic(
+  () => import("@/components/ui/custom/Countdown/Countdown"),
+  { ssr: false },
+);
 
 interface InputData {
   [key: string]: string;
 }
 
 export default function ContactForm() {
+  const [submitting, setSubmitting] = useState(false);
+
   const submitHandler = async (formData: InputData[]) => {
     try {
       const data = {
@@ -32,22 +42,32 @@ export default function ContactForm() {
 
   return (
     <Form
-      onSubmit={submitHandler}
+      onSubmit={() => console.log("sending")}
       render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <fieldset className="flex flex-col">
-            <legend className="font-semibold">Napisz do nas!</legend>
-            {formConfig.map((inputEl) => (
-              <FormField fieldData={inputEl} key={inputEl.name} />
-            ))}
-          </fieldset>
-          <button
-            type="submit"
-            className="w-auto self-start rounded-[var(--small-border-radius)] border border-[var(--magenta-100)] px-6 py-2 hover:cursor-pointer hover:bg-[var(--magenta-100)]"
-          >
-            Wyślij
-          </button>
-        </form>
+        <>
+          <form onSubmit={handleSubmit} className="relative flex flex-col">
+            <fieldset className="flex flex-col">
+              <legend className="font-semibold">Napisz do nas!</legend>
+              {formConfig.map((inputEl) => (
+                <FormField fieldData={inputEl} key={inputEl.name} />
+              ))}
+            </fieldset>
+            <button
+              onClick={() => {
+                setSubmitting(true);
+              }}
+              className="w-auto self-start rounded-[var(--small-border-radius)] border border-[var(--magenta-100)] px-6 py-2 hover:cursor-pointer hover:bg-[var(--magenta-100)]"
+            >
+              Wyślij
+            </button>
+            {submitting && (
+              <div className="absolute inset-0 flex flex-col items-center justify-center bg-[var(--black-100)] opacity-90">
+                <Loader />
+                <p>Wysyłanie...</p>
+              </div>
+            )}
+          </form>
+        </>
       )}
     />
   );
---
+++ src/components/ui/custom/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+export default function Loader() {
+  return (
+    <div className="flex h-[50%] w-[50%] items-center justify-center">
+      <div className="relative inline-block h-40 w-40">
+        <div className="animate-lds-ripple absolute rounded-full border-[4px] border-white bg-[var(--magenta-100)] opacity-100" />
+        <div className="animate-lds-ripple absolute rounded-full border-[4px] border-white bg-[var(--magenta-100)] opacity-100 delay-[-500ms]" />
+      </div>
+    </div>
+  );
+}
