7d699cedc3ff756c956ad0f64bc9492ecf261563
Michal <noszczykmichal@wp.pl>
2025-06-20 16:03:42 +0200

    first draft

--- src/components/Layout/Navigation/SideNav/SideNav.tsx
+++ src/components/Layout/Navigation/SideNav/SideNav.tsx
@@ -3,22 +3,29 @@
 import { useEffect, useContext, useRef } from "react";
 import { createPortal } from "react-dom";
 import { CSSTransition } from "react-transition-group";
+import Link from "next/link";
 
 import Backdrop from "@/components/Layout/Navigation/SideNav/Backdrop/Backdrop";
 import UIContext from "@/store/uiContext";
-import NavigationItems from "../NavigationItems/NavigationItems";
-import useMobileNav from "@/hooks/useMobileNav";
+// import NavigationItems from "../NavigationItems/NavigationItems";
+// import useMobileNav from "@/hooks/useMobileNav";
 import { ListItemData } from "@/types/types";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 interface SideNavProps {
   onBackdropClick: () => void;
   navData: Partial<ListItemData>[];
 }
 
-function SideNav({ onBackdropClick, navData }: SideNavProps) {
+function SideNav({ onBackdropClick }: SideNavProps) {
   const { isMenuOpen, closeSideNavHandler } = useContext(UIContext);
   const nodeRef = useRef<HTMLElement | null>(null);
-  const { onClickHandler } = useMobileNav();
+  // const { onClickHandler } = useMobileNav();
 
   useEffect(() => {
     const onResize = (e: UIEvent) => {
@@ -58,13 +65,57 @@ function SideNav({ onBackdropClick, navData }: SideNavProps) {
           className="xs:w-[60vw] fixed top-0 right-0 z-[12] flex h-[100vh] w-[80vw] flex-col justify-center bg-white shadow-[var(--custom-box-shadow)] sm:w-[50vw]"
           ref={nodeRef}
         >
-          <nav className="flex h-full w-full flex-col items-center justify-center">
-            <NavigationItems
+          <nav className="flex-start flex h-1/2 w-full flex-col items-center">
+            {/* <NavigationItems
               className="flex h-[50%] w-[50%] flex-col justify-around"
               onClick={onClickHandler}
               navData={navData}
               classForDropDown="top-[30%] left-[-50%] text-[16px] xxs:text-[18px] "
-            />
+            /> */}
+
+            <Accordion type="multiple" className="top-0 left-0 w-1/2">
+              <AccordionItem value="about">
+                <Link href="/0-nas" className="block py-1 pl-4 text-sm">
+                  O nas
+                </Link>
+              </AccordionItem>
+
+              <AccordionItem value="treatments">
+                <AccordionTrigger>Zabiegi</AccordionTrigger>
+                <AccordionContent>
+                  <Link
+                    href="/zabiegi/skora"
+                    className="block py-1 pl-4 text-sm"
+                  >
+                    Na twarz
+                  </Link>
+                  <Link
+                    href="/zabiegi/cialo"
+                    className="block py-1 pl-4 text-sm"
+                  >
+                    Na ciało
+                  </Link>
+                </AccordionContent>
+              </AccordionItem>
+
+              <AccordionItem value="blog">
+                <AccordionTrigger>Blog</AccordionTrigger>
+                <AccordionContent>
+                  <Link href="/blog" className="block py-1 pl-4 text-sm">
+                    Wszystkie wpisy
+                  </Link>
+                </AccordionContent>
+              </AccordionItem>
+
+              <AccordionItem value="contact">
+                <AccordionTrigger>Kontakt</AccordionTrigger>
+                <AccordionContent>
+                  <Link href="/kontakt" className="block py-1 pl-4 text-sm">
+                    Formularz
+                  </Link>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </nav>
         </aside>
       </CSSTransition>
