a44c29d235b1426da5977a8f4de0e357d3869179
Michal <noszczykmichal@wp.pl>
2025-06-27 00:13:27 +0200

    cleanup

--- src/store/uiContext.tsx
+++ src/store/uiContext.tsx
@@ -33,7 +33,6 @@ export const UIContextProvider: FC<UIContextProviderProps> = ({ children }) => {
   const context = useMemo(
     () => ({
       isMenuOpen,
-
       menuToggleHandler,
       closeSideNavHandler,
     }),
