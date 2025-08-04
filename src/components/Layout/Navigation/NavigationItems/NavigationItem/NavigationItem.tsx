74a528b4cfe8959783e1ccb38f24c230ba70741e
Michal <noszczykmichal@wp.pl>
2025-08-04 10:50:50 +0200

    minor styling fix

--- src/components/Layout/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
+++ src/components/Layout/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -33,7 +33,7 @@ export default function NavigationItem({
 
   const linkClasses =
     "lg:px-[20px] hover:!bg-transparent focus:!bg-transparent active:!bg-transparent font-normal py-0";
-  const contentClasses = `relative whitespace-nowrap before:absolute before:bottom-[-5px] py-[10px] before:left-0 before:h-[1px] before:w-[0px] before:bg-[var(--magenta-100)] before:transition-all before:duration-300 before:content-[''] hover:text-[var(--magenta-100)] hover:before:w-full active:before:w-full focus:before:w-full`;
+  const contentClasses = `relative whitespace-nowrap before:absolute before:bottom-[-5px] py-[10px] before:left-0 before:h-[1px] before:bg-[var(--magenta-100)] before:transition-all before:duration-300 before:content-[''] hover:text-[var(--magenta-100)] hover:before:w-full active:before:w-full focus:before:w-full`;
 
   const activeLinkClasses =
     `/${pathname.split("/")[1]}` === href
